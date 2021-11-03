62eb5cfd9c0945278fef26a1c2ed1eed4076934b
Vardaan <vardaan13121997@gmail.com>
2021-11-03 16:47:09 +0530

    indexing the queries

--- 4-natours/starter/dev-data/data/import-dev-data.js
+++ 4-natours/starter/dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../Models/tourModel');
+const Review = require('../../Models/reviewModel');
+const User = require('../../Models/userModel');
 
 dotenv.config({
     path: './config.env',
@@ -14,10 +16,18 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB).then(() => console.log(`DB connection succesful`));
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+    fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 const importData = async () => {
     try {
         await Tour.create(tours);
+        await User.create(users, {
+            validateBeforeSave: false,
+        });
+        await Review.create(reviews);
         console.log('Data loaded');
     } catch (error) {
         console.error(error);
@@ -28,6 +38,8 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Tour.deleteMany();
+        await User.deleteMany();
+        await Review.deleteMany();
         console.log('Data deleted');
     } catch (error) {
         console.error(error);
